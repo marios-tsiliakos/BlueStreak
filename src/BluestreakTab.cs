b96cbb34a0718f5e7085dac6166085d5b7963507
Marios Tsiliakos <m.tsiliakos@gmail.com>
2017-12-12 00:10:32 +0000

    First commit of the VS project

---
+++ src/BluestreakTab.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Grasshopper.Kernel;
+
+namespace Bluestreak
+{
+    class BluestreakTab: GH_AssemblyPriority
+    {
+
+        public override GH_LoadingInstruction PriorityLoad()
+        {
+            Grasshopper.Instances.ComponentServer.AddCategoryShortName("Bluestreak", "BS");
+            Grasshopper.Instances.ComponentServer.AddCategorySymbolName("Bluestreak",'B');
+            //Grasshopper.Instances.ComponentServer.AddCategoryIcon("NudiBranch", My.Resources.myicon);
+            return GH_LoadingInstruction.Proceed;
+        }
+    }
+}
