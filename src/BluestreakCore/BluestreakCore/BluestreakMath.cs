4550181298e50437c0ebac82c22d5a6c438bfd70
Marios Tsiliakos <m.tsiliakos@gmail.com>
2017-12-16 18:49:47 +0000

    Updated Bluestreacore solution

---
+++ src/BluestreakCore/BluestreakCore/BluestreakMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluestreakCore
+{
+    /// <summary>   
+    /// BlueStreakMath Class is the class incorporating all the methods, fields and properties for the manipulation of data fetched from the net.
+    /// </summary>  
+    class BluestreakMath
+    {
+        public BluestreakMath() // empty constructor for BluestreakMath
+        {
+        }
+
+        public string Info()
+        {
+            string description = "This Class handles all the web querying routines";
+            return description;
+        }
+    }
+}
