7c1f4b4efa09201c93b38f2c5694af9e578c025d
Marios Tsiliakos <m.tsiliakos@gmail.com>
2017-12-16 17:51:25 +0000

    Added BluestreakGH dir

---
+++ src/BlueStreakGH/BluestreakInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Bluestreak
+{
+    public class BluestreakInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Bluestreak";
+            }
+        }
+        public override string Version
+        {
+            get
+            {
+                return "1.00";
+;            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Bluestreak icon 24x24 colour
+                //return my.Resources.bluestreakIcon;
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                return "BlueStreak offers a a direct link to your cryptocurrency rates within your favourite visual programming platform";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("a73101fa-c455-4aff-b2fc-2cd7e9ccb956");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                return "Marios Tsiliakos, Tarek Hassan, Fillipos Fillipidis";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                return "www.digitalsubstance.wordpress.com/";
+            }
+        }
+    }
+}
---
+++ src/BlueStreakGH/Components/BluestreakCryptos.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+using Grasshopper.Kernel;
+//if we were to add graphical imput
+using Rhino.Geometry;
+
+
+namespace Bluestreak
+{
+    public class BluestreakCryptos : GH_Component
+    {
+        /// <summary>
+        /// A component to track your favourite cryprocurency market rates
+        /// Fetching data from coinmarketcap.com 
+        /// top 10 cryptos are pre-selected, use a custom string to target one thats not available
+        /// refresh rate 30000 ms otherwise your IP address will be blocked
+        /// </summary>
+        public BluestreakCryptos()
+          : base("Cryptos", "Cryptos",
+              "Track your favourite cryptocurrency",
+              "Extra", "Bluestreak")
+        {
+        }
+
+        /// <summary>
+        /// Register inputs.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Register Outputs.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.Register_StringParam("debug", "debugging", "this outputs a random message for debugging the code");
+        }
+
+        /// <summary>
+        /// Main Routine
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string myMes;
+            myMes = "this is just a debugging test";
+
+            DA.SetData(0, myMes);
+        }
+
+        /// <summary>
+        ///component Icon
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // ToDO: Icon development
+                //return Resources.blueStreakIcon;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Unique Guid
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("b87d5f94-d4e9-4afc-89f0-e0570daf495b"); }
+        }
+    }
+}
